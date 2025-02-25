5b8a3e93fb025fe497ce065ea90fd7bb2ec43615
gancs-szily <71602533755@szily.hu>
2025-02-25 13:05:18 +0100

    games.php kész a testre

--- games.php
+++ games.php
@@ -22,4 +22,15 @@ class Games{
         $dbvez->closeDB();
         return $this->games;
     }
+
+    public function getGamesByType($typename){
+        $query="SELECT g_ID,name,pic FROM games
+                INNER JOIN type on games.type_iD=type.t_ID
+                WHERE type.name='".$typename."'";
+        $dbvez=new DBvezerlo();
+        $this->games=$dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+        return $this->games;
+
+    }
 }
